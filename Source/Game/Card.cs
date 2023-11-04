1b5aed7c01d869164a3152d7881f70e1352b56e3
NarraAtor <eff3587@rit.edu>
2023-11-04 13:51:31 -0400

    Doing some stuff wih cards and enums

--- Source/Game/Card.cs
+++ Source/Game/Card.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using FlaxEngine;
+using FlaxEngine.GUI;
 
 namespace Game
 {
+    public enum CardType
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
     /// <summary>
     /// Card Script.
     /// </summary>
     public class Card : Script
     {
+        private CardType cardType;
+        private Image image;
+        public CardType CardType { get { return cardType; } set { cardType = value; } }
+
         /// <inheritdoc/>
         public override void OnStart()
         {
+            image = Actor.GetScript<Image>();
             // Here you can add code that needs to be called when script is created, just before the first game update
-            Console.WriteLine("I started!!!!");
+            SetCardColor();
         }
         
         /// <inheritdoc/>
@@ -32,8 +45,25 @@ namespace Game
         public override void OnUpdate()
         {
             // Here you can add code that needs to be called every frame
-            Console.WriteLine("I'm updating!!!!");
 
+        }
+
+        private void SetCardColor()
+        {
+            switch (cardType)
+            {
+                case CardType.Rock:
+                    image.Color = Color.Green;
+
+                    break;
+                case CardType.Paper:
+                    image.Color = Color.Green;
+                    break;
+                case CardType.Scissors:
+                    image.Color = Color.Green;
+
+                    break;
+            }
         }
     }
 }
