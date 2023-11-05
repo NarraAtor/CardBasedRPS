52f038f759ea48c30abd345a49b74a018d15bd43
NarraAtor <eff3587@rit.edu>
2023-11-05 01:58:11 -0400

    Messed around with buttons and Card prefab

---
+++ Source/Game/TestButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace Game
+{
+    /// <summary>
+    /// TestButton Script.
+    /// </summary>
+    public class TestButton : Script
+    {
+        public Button button;
+        /// <inheritdoc/>
+        public override void OnStart()
+        {
+            // button = Actor.GetGet<Button>();
+            Debug.Log(this.);
+            // Here you can add code that needs to be called when script is created, just before the first game update
+        }
+        
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+            // Here you can add code that needs to be called when script is enabled (eg. register for events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnUpdate()
+        {
+            // Here you can add code that needs to be called every frame
+        }
+    }
+}
